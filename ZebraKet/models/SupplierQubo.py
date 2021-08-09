7b8bd5b1190a4b1abcfc43f898a8dce0df9b1bf6
TangrisJones <cleland.theo@gmail.com>
2021-08-08 23:13:54 -0400

    updated cover problem qubo

--- ZebraKet/models/SupplierQubo.py
+++ ZebraKet/models/SupplierQubo.py
@@ -39,6 +39,64 @@ class SupplierQubo(AbstractQubo):
         self.qubo = self.construct_bqm()
 
     def construct_bqm(self):
+        """Construct BQM for the set cover problem
+        Args:
+            U (array-like):
+                A set of elements defining the universe
+            V (array of sets):
+                Array of subsets
+        Returns:
+            Binary quadratic model instance
+            x: variable
+        """
+
+        U = self.inventory
+        V = self.supplier_inventory
+        A = self.lagrange_a
+        B = self.lagrange_b
+        
+        # Create indicator variables
+        self.indicators = []
+        for i in range(len(V)):
+            self.indicators.append([1 if U[a] in V[i] else 0 for a in range(len(U))])
+        
+        ##@  Binary Quadratic Model @##
+        bqm = BinaryQuadraticModel('BINARY')
+
+        # Add linear terms
+        # x linear terms
+        self.x = [bqm.add_variable('x_'+str(i+1), A*sum(self.indicators[i])+B) for i in range(0,len(V))]
+        
+        # y_am linear terms
+        y = []
+        for a in range(1,len(U)+1):
+            y.append([bqm.add_variable('y_('+str(a)+', '+str(m)+')', A*(m**2-1)) for m in range(1,len(V)+1)])
+        
+        # Add quadratic terms
+
+        # x_i-x_j terms
+        for i in range(1,len(V)+1):
+            for j in range(i+1,len(V)+1):
+                key = ('x_' + str(i), 'x_' + str(j))
+                bqm.quadratic[key] = 2*A*np.dot(np.array(self.indicators[i-1]),np.array(self.indicators[j-1]))
+
+        # y_am - y_an terms
+        for m in range(1,len(V)+1):
+            for n in range(m+1,len(V)+1):
+                for a in range(1,len(U)+1):
+                    key = ('y_('+str(a)+', '+str(m)+')', 'y_('+str(a)+', '+str(n)+')')
+                    bqm.quadratic[key] = 2*A*(1+m*n)
+
+        # x_i-y_am terms
+        for i in range(1,len(V)+1):
+            for m in range(1,len(V)+1):
+                for a in range(1,len(U)+1):
+                    key = ('x_' + str(i), 'y_('+str(a)+', '+str(m)+')')
+                    bqm.quadratic[key] = -2*A*m*self.indicators[i-1][a-1]
+
+        return bqm
+
+    def construct_bqm_old(self):
         """Construct BQM for the generalized set cover problem
         Args:
             
@@ -100,17 +158,20 @@ class SupplierQubo(AbstractQubo):
 if __name__ == "__main__":
 
     # from dimod import ExactSolver
+    from dwave.system import LeapHybridSampler
     from neal import SimulatedAnnealingSampler
     from utils.data import read_inventory_optimization_data
     from config import standard_mock_data
     
     inventory, supplier_inventories = read_inventory_optimization_data(standard_mock_data['small'])
-    sampler = SimulatedAnnealingSampler().sample
+    # sampler = SimulatedAnnealingSampler().sample
+    sampler = LeapHybridSampler().sample
 
     qubo = SupplierQubo(inventory, supplier_inventories)
-    qubo.solve(sampler, **{"num_reads":100, "num_sweeps": 100})
+    # qubo.solve(sampler, **{"num_reads":100, "num_sweeps": 100})
+    qubo.solve(sampler)
 
-    print(qubo.response)
+    # print(qubo.response)
 
     print('\nLength of solution', len(qubo.solution_set[0]))
     print(f'Found {np.sum(qubo.solution_set[0])} suppliers with energy {qubo.energy_set[0]}')
